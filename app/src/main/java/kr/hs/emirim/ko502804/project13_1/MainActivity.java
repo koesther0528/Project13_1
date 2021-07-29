6ed9ea99f3b6d68549e7ac3b26cbffa37b05e69e
esthe <ko990727@hmail.com>
2021-07-29 13:50:43 +0900

    Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project13_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project13_1/MainActivity.java
@@ -23,72 +23,85 @@ public class MainActivity extends AppCompatActivity {
     ListView list1;
     TextView textMusic;
     ProgressBar proBar;
-    ArrayList<String> arrayList;
+    ArrayList<String> arrList;
     String selectedMusic;
     String musicPath = Environment.getExternalStorageDirectory().getPath() + "/";
     MediaPlayer media;
-    Button btnStart, btnStop;
+    Button btnStart, btnStop, btnPause;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("MP3 Player");
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MODE_PRIVATE);
-        arrayList = new ArrayList<String>();
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
+        arrList = new ArrayList<String>();
         File[] listFiles = new File(musicPath).listFiles();
         String fileName, extName;
-        for(File file : listFiles){
+        for (File file : listFiles){
             fileName = file.getName();
             extName = fileName.substring(fileName.length()-3);
-            if(extName.equals("mp3"))
-                arrayList.add(fileName);
+            if (extName.equals("mp3"))
+                arrList.add(fileName);
         }
         list1 = findViewById(R.id.list1);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, arrayList);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice,arrList);
         list1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        list1.setItemChecked(0, true);
+        list1.setAdapter(adapter);
+        list1.setItemChecked(0,true);
 
-        list1.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l){
-                selectedMusic = arrayList.get(i);
-                return false;
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                selectedMusic = arrList.get(position);
             }
         });
-        selectedMusic = arrayList.get(0);
+        selectedMusic = arrList.get(0);
 
-        btnStart = findViewById(R.id.btn_start);
-        btnStop = findViewById(R.id.btn_stop);
+        Button btnStart = findViewById(R.id.btn_start);
+        Button btnstop = findViewById(R.id.btn_stop);
         textMusic = findViewById(R.id.text_music);
         proBar = findViewById(R.id.progress);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                media = new MediaPlayer();
                 try {
-                    media = new MediaPlayer();
                     media.setDataSource(musicPath + selectedMusic);
                     media.prepare();
                     media.start();
                     btnStart.setClickable(false);
                     btnStop.setClickable(true);
-                    textMusic.setText(selectedMusic + ":");
-                    proBar.setVisibility(View.INVISIBLE);
-
-                } catch (IOException e) {
+                    textMusic.setText(selectedMusic);
+                    proBar.setVisibility(View.VISIBLE);
+                } catch (IOException e){
                     e.printStackTrace();
                 }
             }
         });
-        btnStop.setOnClickListener(new View.OnClickListener() {
+        btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                if(btnPause.getText().equals("일시 중지")){
+                    media.pause();
+                    btnPause.setText("이어듣기");
+                    proBar.setVisibility(View.INVISIBLE);
+                }else if(btnPause.getText().equals("이어 듣기")){
+                    media.start();
+                    btnPause.setText("일시 중지");
+                    proBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        btnstop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 media.stop();
                 media.reset();
                 btnStart.setClickable(true);
-                btnStop.setClickable(false);
-                textMusic.setText("실행음악 중지");
+                btnstop.setClickable(false);
+                textMusic.setText("실행음악 중지: ");
                 proBar.setVisibility(View.INVISIBLE);
             }
         });
